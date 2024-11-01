94025211b8437ef35626f12f178efdc6501ce0d5
magnus32 <bruemmersven@gmx.de>
2024-11-01 12:46:51 +0100

    fix

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config:
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = ''
     MYSQL_DB = 'adressbuch_db'
-    MYSQL_CURSORCLASS = 'DictCursor'  # Optional: Für eine bessere Ausgabe
+    MYSQL_CURSORCLASS = 'DictCursor'  # Optional: Für eine bessere Ausgabemodels
